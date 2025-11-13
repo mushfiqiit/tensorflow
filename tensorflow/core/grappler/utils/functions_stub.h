98a0f2fe38fbcbda24a482c6ce297d54b18bc26e
mushfiqiit <bsse1110@iit.du.ac.bd>
2025-11-13 22:35:05 +0600

    Working on harness

---
+++ tensorflow/core/grappler/utils/functions_stub.h
@@ -0,0 +1,9 @@
+
+
+Status MakeGrapplerFunctionItem(const FunctionDef& func,
+                                const AttrSlice& func_instantiation_attr,
+                                const FunctionLibraryDefinition& flib,
+                                const int graph_def_version,
+                                GrapplerFunctionItem* item) {
+
+}
